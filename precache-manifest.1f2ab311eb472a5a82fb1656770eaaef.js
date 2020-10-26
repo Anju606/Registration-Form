8f399804fffe99f777cd84b13e72a705db281727
Anju Panicker <panikeranju@gmail.com>
2020-10-26 20:06:48 +0530

    Updates

---
+++ precache-manifest.1f2ab311eb472a5a82fb1656770eaaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f8b7e36868b0da3be8b8cb1481fc6e",
+    "url": "/Registration-Form/index.html"
+  },
+  {
+    "revision": "d613857a6cb59cb0a855",
+    "url": "/Registration-Form/static/css/main.e3214337.chunk.css"
+  },
+  {
+    "revision": "4ee418381c86ddfaf9d0",
+    "url": "/Registration-Form/static/js/2.34653e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Registration-Form/static/js/2.34653e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d613857a6cb59cb0a855",
+    "url": "/Registration-Form/static/js/main.0cdda54d.chunk.js"
+  },
+  {
+    "revision": "2ebe35d6fe3a32383ddd",
+    "url": "/Registration-Form/static/js/runtime-main.1f217cbe.js"
+  }
+]);
